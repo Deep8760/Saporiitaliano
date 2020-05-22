6d5a7e5867a6fe02d5ad2bd7da03d14f20c42bda
deepak maan <lluckymaan8787@gmail.com>
2020-05-22 00:58:00 -0400

    intial code

--- app/src/main/java/com/example/saporiitaliano/MainActivity.java
+++ app/src/main/java/com/example/saporiitaliano/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.saporiitaliano;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    EditText text1,text2,deep;
+    Button Login,forgetpass,signup;
+    private FirebaseAuth firebaseAuth;
+    public void deep(View abc){
+        startActivity(new Intent(getApplicationContext(),Forgetpass.class));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text1=(EditText)findViewById(R.id.email);
+        text2=(EditText)findViewById(R.id.pass);
+        signup=(Button) findViewById(R.id.button5);
+forgetpass=(Button)findViewById(R.id.button4);
+        Login=(Button)findViewById(R.id.login) ;
+
+        firebaseAuth=firebaseAuth.getInstance();
+
+signup.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        startActivity(new Intent(getApplicationContext(),Registeruser.class));
     }
-}
+});
+            }
+
+    }
+
